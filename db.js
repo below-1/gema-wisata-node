93063273a9eb15af27e6ad4d7c36ee14bd32eca6
Below Bed <jordanmeta4677@gmail.com>
2022-05-05 17:27:28 +0800

    dynamic mongo_uri

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 import fp from 'fastify-plugin'
 
+const mongo_uri = process.env.MONGO_URI || 'mongodb://localhost/gema_db'
+
 export default fp(async (fastify) => {
   try {
-    await mongoose.connect('mongodb://localhost/gema_db')
+    await mongoose.connect(mongo_uri)
     fastify.decorate('get_mongo_client', mongoose.connection.getClient())
     console.log(`done connection to mongo`)
   } catch (err) {
